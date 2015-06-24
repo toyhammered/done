b5449f06eff729214056f81f609bd78589fbd6a1
toyhammered <daniel@rassiner.com>
2015-06-24 16:44:02 -0700

    Added link detection

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,12 @@
 class Task < ActiveRecord::Base
 	belongs_to :user
+
+	# where is it going to get the raw html in the browser so it can display it
+	auto_html_for :content do 
+		html_escape
+		image
+		youtube(:width => "100%", :height => 250, :autoplay => false)
+		link :target => "_blank", :rel => "nofollow"
+		simple_format
+	end
 end
